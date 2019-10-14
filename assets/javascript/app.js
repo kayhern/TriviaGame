a6b7e9798844ca5dd3ff1350dcb786783084f8d5
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-14 19:44:24 -0400

    pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,23 @@
 //first window pops up - start button
 
-//when start button is clicked: first window disappears, second window pops up
+//when start button is clicked: first window disappears, second window pops up, timer begins
 
-//
+//timer is counting down from two minutes; when timer goes up the player loses
+
+//when user selects answer to question1 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question2 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question3 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question4 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question5 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question6 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question7 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user selects answer to question8 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
+
+//when user clicked finish: popup window showing number of correct, number of incorrect, number of unanswered
