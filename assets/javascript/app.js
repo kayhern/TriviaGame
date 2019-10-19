4473136ad05c89de8456263cdd501080c80f30cb
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-19 09:42:14 -0400

    struggling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,79 @@
+//I am going to make a huge document.ready function that contains:
+//-trivia game
+//--within trivia game contains questions, ability to diplay questions, show resules, show timer
+
 $(document).ready(function(){
+//source for javascript multiple choice questions to appear as a variable: https://www.sitepoint.com/simple-javascript-quiz/
 
-    let numberCorrect = 0;
-    let numberIncorrect = 0;
-    let numberUnanswered = 0;
-    let timeQuiz = 1200;
+function theEntireGame(){
 
- 
+let numberCorrect = 0;
+let numberIncorrect = 0;
+let numberUnanswered = 0;
+let timeQuiz = 1200;
+
+const quizQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously",
+        d: ""
+      },
+      correctAnswer: ""
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best",
+        d: ""
+      },
+      correctAnswer: ""
+    },
+    {
+      question: "Where is Waldo really?",
+      answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+      },
+      correctAnswer: "d"
+    },
+    {
+        question: "Where is Waldo really?",
+        answers: {
+          a: "Antarctica",
+          b: "Exploring the Pacific Ocean",
+          c: "Sitting in a tree",
+          d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+      },
+  ];
+
+//add the timer somehow????
+
+ //make the questions appear on the page using for Each method https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach?v=control
     function showQuestions() {
 
     };
-    showQuestions();
 
-    function showResults() {
+//container to show results used when user clicked finish in the other function below
+    function results() {
 
     };
-    showResults();
+
+//when user clicked finish: popup window showing number of correct, number of incorrect, number of unanswered or when the last answer is finished the results appear?
+//source: https://www.geeksforgeeks.org/how-to-clear-the-content-of-a-div-using-javascript/
+//--REMOVE CHILD TO REMOVE QUESTIONS
+    $("#complete-game-button").click(function() {
+        results();
+    });
+};
 
 //first window pops up - start button - make it disappear on click function
 //source: https://www.youtube.com/watch?v=RCFzfym8RvU
@@ -28,12 +87,6 @@ $(document).ready(function(){
         // let elementAppear = document.getElementById("#div.quiz");
         showQuestions();
     });
-
-    $("#complete-game-button").click(function() {
-        showResults();
-    };
-
-
 });
 
 
@@ -49,12 +102,3 @@ $(document).ready(function(){
 
 //when user selects answer to question5 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
 
-//when user selects answer to question6 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
-
-//when user selects answer to question7 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
-
-//when user selects answer to question8 : if answer is correct then corect number goes up else incorrect answer goes up or else unanswered goes up
-
-//when user clicked finish: popup window showing number of correct, number of incorrect, number of unanswered
-//source: https://www.geeksforgeeks.org/how-to-clear-the-content-of-a-div-using-javascript/
-//--REMOVE CHILD TO REMOVE QUESTIONS
